7dfec3025429f959cc422696d08b5ad0601c613b
anitaforte <70827757+anitaforte@users.noreply.github.com>
2021-03-24 11:41:16 +1100

    Update 9

--- js/index.js
+++ js/index.js
@@ -1,11 +1,4 @@
 
 console.log("Hello world!");
 
-$("#myButton").ready(function(){
-  $("#myButton").click(function(){
-    $('#myDiv').animate({
-      'font-size': 50
-    }, 2000);
-  });
-});
 
