080f9abd67a12818532deb92ea11f6435bef5868
anitaforte <70827757+anitaforte@users.noreply.github.com>
2021-03-25 20:04:35 +1100

    Update 10

--- js/index.js
+++ js/index.js
@@ -1,4 +1,21 @@
 
 console.log("Hello world!");
 
+$(footerUl).ready(function(){
+  $("#connect").toggle("scale",{
+    percent: "80%"
+  },0);
+$(footerUl).ready(function(){
+  $("#connect").toggle("scale",{
+    percent: "120%"
+  },0);
+$(footerUl).ready(function(){
+    $("#connect").toggle("scale",{
+      percent: "80%"
+    },0);
+});
+
+
+
+
 
